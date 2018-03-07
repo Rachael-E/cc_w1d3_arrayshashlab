83f31c3d42e274f9c50b057b0d6269d114a17ffe
Rachael <rachael.ellen@btopenworld.com>
2018-03-07 22:25:31 +0000

    Completed the homework finally

--- homework.rb
+++ homework.rb
@@ -54,72 +54,130 @@
 
 ### Homework B - HASHES ###
 
-  users = {
-    "Jonathan" => {
-      :twitter => "jonnyt",
-      :favourite_numbers => [12, 42, 75, 12, 5],
-      :home_town => "Stirling",
-      :pets => {
-        "fluffy" => :cat,
-        "fido" => :dog,
-        "spike" => :dog
-      }
-    },
-    "Erik" => {
-      :twitter => "eriksf",
-      :favourite_numbers => [8, 12, 24],
-      :home_town => "Linlithgow",
-      :pets => {
-        "nemo" => :fish,
-        "kevin" => :fish,
-        "spike" => :dog,
-        "rupert" => :parrot
-      }
-    },
-    "Avril" => {
-      :twitter => "bridgpally",
-      :favourite_numbers => [12, 14, 85, 88],
-      :home_town => "Dunbar",
-      :pets => {
-        "colin" => :snake
-      }
-    },
+#   users = {
+#     "Jonathan" => {
+#       :twitter => "jonnyt",
+#       :favourite_numbers => [12, 42, 75, 12, 5],
+#       :home_town => "Stirling",
+#       :pets => {
+#         "fluffy" => :cat,
+#         "fido" => :dog,
+#         "spike" => :dog
+#       }
+#     },
+#     "Erik" => {
+#       :twitter => "eriksf",
+#       :favourite_numbers => [8, 12, 24],
+#       :home_town => "Linlithgow",
+#       :pets => {
+#         "nemo" => :fish,
+#         "kevin" => :fish,
+#         "spike" => :dog,
+#         "rupert" => :parrot
+#       }
+#     },
+#     "Avril" => {
+#       :twitter => "bridgpally",
+#       :favourite_numbers => [12, 14, 85, 88],
+#       :home_town => "Dunbar",
+#       :pets => {
+#         "colin" => :snake
+#       }
+#     },
+#   }
+#
+# # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+# p users["Jonathan"][:twitter]
+#
+# # 2. Get Erik's hometown
+# p users["Erik"][:home_town]
+#
+# # 3. Get the array of Erik's favourite numbers
+# p  users["Erik"][:favourite_numbers]
+#
+# # 4. Get the type of Avril's pet Colin
+# p users["Avril"][:pets]
+#
+# # 5. Get the smallest of Erik's favourite numbers
+# p users["Erik"][:favourite_numbers].min
+#
+# # 6. Add the number `7` to Erik's favourite numbers
+# users["Erik"][:favourite_numbers].insert(0, 7)
+#
+# p users["Erik"][:favourite_numbers]
+#
+# # 7. Change Erik's hometown to Edinburgh
+#
+# users["Erik"][:home_town].replace ("Edinburgh")
+#
+# p users["Erik"][:home_town]
+# # 8. Add a pet dog to Erik called "Fluffy"
+# users["Erik"][:pets]
+#
+# new_pet = {"Fluffy" => :dog}
+# users["Erik"][:pets].merge!(new_pet)
+#
+# p users["Erik"][:pets]
+# # 9. Add yourself to the users hash
+# new_user = {"Rachael" => {}}
+# users.merge!(new_user)
+#
+# p users
+
+### Homework C ###
+
+united_kingdom = [
+  {
+    name: "Scotland",
+    population: 5295000,
+    capital: "Edinburgh"
+  }, {
+    name: "Wales",
+    population: 3063000,
+    capital: "Swansea"
+  }, {
+    name: "England",
+    population: 53010000,
+    capital: "London"
   }
+]
 
-# 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-p users["Jonathan"][:twitter]
+# 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 
-# 2. Get Erik's hometown
-p users["Erik"][:home_town]
+united_kingdom[1][:capital] = "Cardiff"
 
-# 3. Get the array of Erik's favourite numbers
-p  users["Erik"][:favourite_numbers]
+    p united_kingdom
 
-# 4. Get the type of Avril's pet Colin
-p users["Avril"][:pets]
+    #or
+    # for country in united_kingdom
+    #   if country[:capital] == "Swansea"
+    #     country[:capital] = "Cardiff"
+    #   end
+    # end
 
-# 5. Get the smallest of Erik's favourite numbers
-p users["Erik"][:favourite_numbers].min
+# 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+united_kingdom[3] = {
+  name: "Northern Ireland",
+  population: 1811000,
+  capital: "Belfast"
+}
 
-# 6. Add the number `7` to Erik's favourite numbers
-users["Erik"][:favourite_numbers].insert(0, 7)
+  p united_kingdom
 
-p users["Erik"][:favourite_numbers]
+#p united_kingdom
 
-# 7. Change Erik's hometown to Edinburgh
+# 3. Use a loop to print the names of all the countries in the UK.
 
-users["Erik"][:home_town].replace ("Edinburgh")
+for country in united_kingdom
+  p country[:capital]
+end
 
-p users["Erik"][:home_town]
-# 8. Add a pet dog to Erik called "Fluffy"
-users["Erik"][:pets]
 
-new_pet = {"Fluffy" => :dog}
-users["Erik"][:pets].merge!(new_pet)
+# 4. Use a loop to find the total population of the UK.
+uk_population = 0
 
-p users["Erik"][:pets]
-# 9. Add yourself to the users hash
-new_user = {"Rachael" => {}}
-users.merge!(new_user)
+for population in united_kingdom
+  uk_population += population[:population]
+end
 
-p users
+  p uk_population
